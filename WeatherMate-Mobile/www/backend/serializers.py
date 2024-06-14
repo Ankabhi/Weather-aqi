af9d6fd5b33f8a7e68348f963f86b95e248f1105
Son Nguyen <hoangson091104@gmail.com>
2024-06-14 14:35:11 +0700

    Delete WeatherMate-Mobile directory
    Signed-off-by: Son Nguyen <hoangson091104@gmail.com>

--- WeatherMate-Mobile/www/backend/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import WeatherData
-
-class WeatherDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WeatherData
-        fields = ['city', 'data', 'timestamp']
